2ab1cfd72e6b133136c4514952f07fab07ccf666
JakeBayer <jacobwbayer@gmail.com>
2017-11-20 21:41:55 -0500

    36

---
+++ ProjectEuler/Problems/1-50/31-40/Problem37/Problem37.cs
@@ -0,0 +1,53 @@
+﻿using ProjectEuler.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    public class Problem37 : IProblem
+    {
+        public string Run()
+        {
+            var primes = Primes.ToLongHash(10000000); // random guess of 10 million being sufficient, not worth my time to continuously generate exact amount
+
+            long i = 9;
+            List<long> ans = new List<long>();
+            while (ans.Count < 11) //problem explicitly states 11 exist
+            {
+                i += 2;
+                long j = i;
+                while (j > 0 && primes.Contains(j))
+                {
+                    j = TruncateLeft(j);
+                }
+                if (j != 0) continue;
+
+                j = i;
+                while (j > 0 && primes.Contains(j))
+                {
+                    j = TruncateRight(j);
+                }
+                if (j != 0) continue;
+                ans.Add(i);
+            }
+
+            return ans.Sum().ToString();
+        }
+
+        private long TruncateLeft(long i)
+        {
+            var truncated = i.ToString().Substring(1);
+            return string.IsNullOrEmpty(truncated) ? 0 : long.Parse(truncated);
+        }
+
+        private long TruncateRight(long i)
+        {
+            var asString = i.ToString();
+            var truncated = asString.Substring(0, asString.Length - 1);
+            return string.IsNullOrEmpty(truncated) ? 0 : long.Parse(truncated);
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -11,7 +11,7 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            var problem = new Problem36();
+            var problem = new Problem37();
             Console.WriteLine(problem.Run());
         }
     }
