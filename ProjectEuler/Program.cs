1ab232da66a994846bdcd5f176c81a56b9014663
JakeBayer <jacobwbayer@gmail.com>
2017-12-18 01:07:28 -0500

    problem 87

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -11,7 +11,7 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            var problem = new Problem85();
+            var problem = new Problem87();
             Console.WriteLine(problem.Run());
         }
     }
