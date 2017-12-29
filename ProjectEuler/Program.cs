e27abd120cb9e358ab026f8690a1aa8b7204384f
JakeBayer <jacobwbayer@gmail.com>
2017-12-29 01:15:27 -0500

    Problem 500. This was much harder than the 15% difficulty rating it was given. Or I didn't see the easy answer :X

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -11,7 +11,7 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            var problem = new Problem206();
+            var problem = new Problem500();
             Console.WriteLine(problem.Run());
         }
     }
