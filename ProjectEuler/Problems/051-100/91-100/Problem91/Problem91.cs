bcbec467ff79cb2fbc78687828913292beda5977
Jake Bayer <jacob.bayer@blackstone.com>
2018-03-28 14:02:33 -0400

    problem 91

--- ProjectEuler/Problems/051-100/91-100/Problem91/Problem91.cs
+++ ProjectEuler/Problems/051-100/91-100/Problem91/Problem91.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectEuler.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,60 @@ namespace ProjectEuler.Problems
     {
         public string Run()
         {
-            throw new NotImplementedException();
+            return CountRightTrianglesOnGridOfSize(50, 50).ToString();
+        }
+
+        public long CountRightTrianglesOnGridOfSize(int maxX, int maxY)
+        {
+            /* Start with the trivial right triangles. Trivial right triangles consist of:
+                1) triangles where both legs lie on the x and y axes (there are x * y of these)
+                2) triangles where one of the legs lies on the x and y axes, and the other vector is the hypotnuse. 
+                    It should be obvious that for every point not on either axis (of which there are x * y), there 
+                    are two associated right triangles (one for each axis). These are the vectors "projected" onto the axes.
+                Therefore there are 3 * x * y trivial right triangles.
+            */
+            long totalRightTriangles = 3 * maxX * maxY;
+
+            // Now we count the non-trivial triangles. (ones where the right angle does not touch either axis.
+            for (int x = 1; x <= maxX; x++)
+            {
+                for (int y = 1; y <= maxY; y++)
+                {
+                    totalRightTriangles += CountLeft(x, y, maxX, maxY) + CountRight(x, y, maxX, maxY);
+                }
+            }
+
+            return totalRightTriangles;
+        }
+
+        private int CountLeft(int x, int y, int maxX, int maxY)
+        {
+            var gcd = x.GCD(y);
+            var dx = y / gcd;
+            var dy = x / gcd;
+            int x2 = x - dx, y2 = y + dy, total = 0;
+            while (x2 >= 0 && y2 <= maxY)
+            {
+                x2 -= dx;
+                y2 += dy;
+                total++;
+            }
+            return total;
+        }
+
+        private int CountRight(int x, int y, int maxX, int maxY)
+        {
+            var gcd = x.GCD(y);
+            var dx = y / gcd;
+            var dy = x / gcd;
+            int x2 = x + dx, y2 = y - dy, total = 0;
+            while (x2 <= maxX && y2 >= 0)
+            {
+                x2 += dx;
+                y2 -= dy;
+                total++;
+            }
+            return total;
         }
     }
 }
